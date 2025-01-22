560a27c6db13472a60e4049b96b9ccfeb94cfd00
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-01-22 12:28:54 +0100

    Write parse function body

--- app/parser.c
+++ app/parser.c
@@ -9,3 +9,33 @@ Token* advance(TokenManager* manager)
   manager->pos++;
   return token;
 }
+
+JSONNode* parse(TokenManager* manager, ParserError* error)
+{
+  if (error && error->type != NO_PARSER_ERROR)
+    return NULL;
+
+  Token* token = advance(manager);
+
+  if (token->type == CURLY_OPEN)
+    return parseObject(manager);
+  if (token->type == BRACKET_OPEN)
+    return parseArray(manager);
+  if (token->type == STRING_LEX)
+    return parseString(token);
+  if (token->type == INTEGER_LEX)
+    return parseInteger(token);
+  if (token->type == DOUBLE_LEX)
+    return parseDouble(token);
+  if (token->type == BOOLEAN_LEX)
+    return parseBoolean(token);
+  if (token->type == NULL_LEX)
+    return parseNull(token);
+
+  if (error)
+  {
+    error->type = UNEXPECTED_TOKEN;
+    error->token = *token; // duplicate token in stack
+    return NULL;
+  }
+}
