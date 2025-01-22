b2f9e3130cf60d5026a80f940aae8159241d09e7
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-01-22 10:46:33 +0100

    Create smart vector allocation function

---
+++ app/utils.c
@@ -0,0 +1,24 @@
+#include "utils.h"
+
+#include <math.h>
+#include <stdlib.h>
+
+void* valloc(void* vec, size_t* cap, const size_t size, const size_t elemSize)
+{
+  if (size == 0 || elemSize == 0)
+    return vec;
+
+  // Funnily enough, there's actually no condition needed to check whether to
+  // resize or not since what we want is to have the minimum possible capacity
+  // to hold size which can be achieved by taking the log2 of the current size,
+  // flooring it then incrementing it by one.
+  *cap = pow(2, floor(log2(size)) + 1);
+
+  void* newVec = realloc(vec, *cap * elemSize);
+
+  // If realloc returns NULL, make sure to free the old memory
+  if (newVec == NULL)
+    free(vec);
+
+  return newVec;
+}
---
+++ app/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stddef.h>
+
+void* valloc(void* vec, size_t* cap, const size_t size, const size_t elemSize);
+
+#endif // UTILS_H
